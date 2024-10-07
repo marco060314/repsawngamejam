11e4c583e9b2c805b59d68ffe1b096e097d148da
la20jose <lamj5861@lgsstudent.org>
2024-10-07 11:54:25 -0500

    added mainmenu

---
+++ GODOT/tester/src/scripts/Menu.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Menu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	public void onPressed(){
+		GD.Print("Start Button Pressed!");
+		GetTree().ChangeSceneToFile("res://src/background/betterBackground.tscn");
+		DifficultySelector ds=GetNode<DifficultySelector>("DifficultySelector");
+		Difficulty d=ds.getDifficulty(); //somehow pass the difficulty to the betterBackground.tscn scene
+	}
+}
