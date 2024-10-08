6002603a004590b4e2e84c8929a080f1fe94ea8a
la20jose <lamj5861@lgsstudent.org>
2024-10-08 17:41:05 -0500

    fix maybe

--- FishTankVersion1/FishTankVersion1/src/scripts/P1Score.cs
+++
@@ -1,7 +0,0 @@
-using Godot;
-using System;
-
-public partial class P1Score : Counter
-{
-	public P1Score(): base("Player 1 Score:"){}
-}
--- FishTankVersion1/FishTankVersion1/src/scripts/P2Score.cs
+++
@@ -1,7 +0,0 @@
-using Godot;
-using System;
-
-public partial class P2Score : Counter
-{
-	public P2Score(): base("Player 2 Score:"){}
-}
